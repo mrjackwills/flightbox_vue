95c7af62d0e57b369ec9cc4723d743b4bf3d6aff
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-02-16 16:48:09 +0000

    fix: sort by methods not in computed

--- src/components/Authenticated/CurrentFlights.vue
+++ src/components/Authenticated/CurrentFlights.vue
@@ -22,7 +22,7 @@
 					<v-row class='ma-0 pa-0 text-left' justify='space-between' align='center'>
 
 						<v-col cols='12' class='ma-0 pa-0'>
-							<section v-for='(flight, index) in current_flights' :key='flight.aircraft.mode_s'>
+							<section v-for='(flight, index) in local_current_flights' :key='flight.aircraft.mode_s'>
 								<AircraftRow :flight='flight' :showDivider='index !== number_current_flights - 1'
 									:index='index' />
 							</section>
@@ -52,9 +52,21 @@ import type { TAdsbdb, TSortBy, u } from '@/types';
 import { mdiArrowDownThick, mdiArrowUpThick, mdiSort } from '@mdi/js';
 const aircraftStore = aircraftModule();
 
+const local_current_flights = ref([] as Array<TAdsbdb>);
+
 const current_flights = computed((): Array<TAdsbdb> => {
 	return aircraftStore.current_flights;
 });
+
+onMounted(() => {
+	local_current_flights.value = [ ...current_flights.value ];
+	sort_local();
+});
+
+// onMounted(() => {
+// 	local
+// })
+
 const init = computed((): boolean => {
 	return aircraftStore.init;
 });
@@ -85,6 +97,7 @@ const sort_by = computed({
 		return aircraftStore.sort_by;
 	},
 	set (b: u<TSortBy>): void {
+		// return;
 		aircraftStore.set_sort_by(b);
 	}
 });
@@ -117,6 +130,69 @@ const headers = [
 	},
 
 ];
+
+const sort_local = (): void => {
+	if (!sort_by.value) {
+		local_current_flights.value.sort((a, b) => {
+			const callsign_a = a.callsign || 'z';
+			const callsign_b = b.callsign || 'z';
+			return callsign_a === callsign_b ? a.altitude > b.altitude ? 1 : 0 : callsign_a > callsign_b ? 1 : 0;
+		});
+	} else {
+		if (sort_by.value === 'altitude') {
+			local_current_flights.value.sort((a, b) => {
+				if (sort_asc.value) {
+					return a.altitude >= b.altitude ? 1 : -1;
+				} else {
+					return a.altitude >= b.altitude ? -1 : 1;
+				}
+			});
+		} else if (sort_by.value === 'aircraft') {
+			local_current_flights.value.sort((a, b) => {
+				const aircraft_a =`${a.aircraft.manufacturer} ${a.aircraft.icao_type}`;
+				const aircraft_b =`${b.aircraft.manufacturer} ${b.aircraft.icao_type}`;
+				if (sort_asc.value) {
+					return aircraft_a.toLowerCase() >= aircraft_b.toLowerCase()? 1 : -1;
+				} else {
+					return aircraft_a.toLowerCase() >=aircraft_b.toLowerCase() ? -1 : 1;
+				}
+			});
+		} else if (sort_by.value === 'owner') {
+			local_current_flights.value.sort((a, b) => {
+				if (sort_asc.value) {
+					return a.aircraft.registered_owner.toLowerCase() >= b.aircraft.registered_owner.toLowerCase() ? 1 : -1;
+				} else {
+					return a.aircraft.registered_owner.toLowerCase() >= b.aircraft.registered_owner.toLowerCase() ? -1 : 1;
+				}
+			});
+		} else {
+			local_current_flights.value.sort((a, b) => {
+				const callsign_a = a.callsign || 'z';
+				const callsign_b = b.callsign || 'z';
+				if (sort_asc.value) {
+					return callsign_a >= callsign_b ? 1 : -1;
+				} else {
+					return callsign_a >= callsign_b ? -1 : 1;
+				}
+			});
+		}
+	}
+};
+
+watch(sort_asc, (_) => {
+	// handle the sort in here
+	sort_local();
+});
+watch(sort_by, (_) => {
+	// handle the sort in here
+	sort_local();
+});
+
+watch(current_flights, (i) => {
+	// handle the sort in here
+	local_current_flights.value = [ ...i ];
+	sort_local();
+});
 
 </script>
 
